e29f94dcccad9390b580da809a4da4d79feecf24
harsh18 <harsh.lamba181189@gmail.com>
2015-05-08 20:23:27 +0530

    commit

--- app/controller.js
+++ app/controller.js
@@ -6,7 +6,35 @@
 
 	filterController.$inject = ['$scope'];
 	function filterController($scope){
-		$scope.select2 = 2;
+		$scope.select2 = 'two';
+		$scope.select2Options = {
+	        allowClear:true
+	    };
+	    $scope.calculationPreview = [
+            {
+                $$hashkey : "ONU",
+                'name' : "Since Inception",
+                'reportTypes' : [
+                    {
+                        's1':'returns',
+                        's2':'Percentile Analysis',
+                        's3':'Cash Flow/NAV sumaary',
+                        's4':'Internal Fund supporting Data'
+                    }
+                ]
+            },
+            {
+                $$hashkey : "ONU",
+                'name' : "Horizon",
+                'reportTypes' : [
+                    {
+                        'h1':'Horizon returns',
+                        'h2':'Horizon Summary',
+                        'h3':'Cash Flow/NAV sumaary'
+                    }
+                ]
+            }
+        ];
 	}
 
 })(this);
